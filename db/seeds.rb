b767024520d51bb440ac5dd3c83726b3d7b18c75
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-11-12 08:03:20 -0600

    store

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    Food.create(name: 'Apple', amount: 2)
